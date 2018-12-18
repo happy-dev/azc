9458a5d555cb5dc621ca65f8dcd6cbefd2a651d1
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2018-12-18 18:37:51 +0100

    install : mise en place saas

--- src/wp-content/themes/azc/functions.php
+++ src/wp-content/themes/azc/functions.php
@@ -1,7 +1,7 @@
 <?php
 function enqueue_styles() {
 
-    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css' );
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
 
 }
--- src/wp-content/themes/azc/index.php
+++ src/wp-content/themes/azc/index.php
@@ -1,5 +1,4 @@
 <?php
-
 get_header();
 ?>
     <section id="primary" class="content-area">
