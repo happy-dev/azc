1d4771ef9b7ad6e56c692bbe674786b1fceac1fa
Nadir <nadir@happy-dev.fr>
2018-12-18 09:31:16 +0100

    install bootstrap

---
+++ src/wp-content/themes/azc/functions.php
@@ -0,0 +1,14 @@
+<?php
+function enqueue_styles() {
+
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
+
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_styles');
+
+add_theme_support( 'custom-background', apply_filters( 'understrap_custom_background_args', array(
+    'default-color' => 'ffffff',
+)));
+
+?>
---
+++ src/wp-content/themes/azc/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title><?php wp_title(); ?></title>
+        <?php wp_head(); ?>
+    </head>
+<body>
