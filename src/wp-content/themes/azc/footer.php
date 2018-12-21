ae04903919fb71ee585087f8001e63525676a867
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2018-12-21 14:42:54 +0100

    ui : footer

--- src/wp-content/themes/azc/footer.php
+++ src/wp-content/themes/azc/footer.php
@@ -1,11 +1,20 @@
         <footer>
-            <div>
-                <img src="" alt="" width="" height="" />
-                <p>
-                    15/17 rue vulpian paris
-                </p>
+            <div class="block-footer">
+				<div class="footer-logo">
+					<p class="azc-logo-footer">AZC</p>
+					<p>Architecte</p>
+				</div>
+				<div class="footer-contact">
+					<p>15/17 rue vulpian 75013 Paris<br/>
+					+33 1 55 25 25 25
+					<a href="mailto:contact@azc.archi">contact@azc.archi</a>
+					<p>credits : AZC 2018</p>
+					<a href="">mentions légales</a>
+				</div>
+				<div class="social-link"></div>
             </div>
         </footer>
         <?php wp_footer(); ?>
     </body>
 </html>
+	
