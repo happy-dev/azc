f83bf9b8178f47e6184c97c707c27dc7ce984860
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2019-01-14 16:21:22 +0100

    footer AZC

--- src/wp-content/themes/azc/footer-azc.php
+++ src/wp-content/themes/azc/footer-azc.php
@@ -1,25 +1,35 @@
         <footer class="footer-bg-white" id="contact">
-            <div class="block-footer">
-                <p>CONTACT</p>
-				<div class="footer-contact">
-					<p>
-                        15/17 rue vulpian
-                        <br/>
-                        75013 Paris
-                        <br/>
-					    T +33 1 55 25 25 25
-                        <br/>
-                        F +33 1 43 73 02 82
-					    <a href="mailto:contact@azc.archi">contact@azc.archi</a>
-                        <br/>
-                        <a href="mailto:com@azc.archi">com@azc.archi</a>
-                        <br/>
-                        <a href="mailto:jobs@azc.archi">jobs@azc.archi</a>
-                    </p>
-					<p>credits : AZC 2018</p>
-					<a href="">mentions légales</a>
-				</div>
-				<div class="social-link"></div>
+            <div class="block-footer-AZC row">
+                <div class="col-md-6 col-12">
+                    <p>CONTACT</p>
+                    <div class="footer-contact">
+                        <p>
+                            15/17 rue vulpian
+                            <br/>
+                            75013 Paris
+                            <br/>
+                            T +33 1 55 25 25 25
+                            <br/>
+                            F +33 1 43 73 02 82
+                            <a href="mailto:contact@azc.archi">contact@azc.archi</a>
+                            <br/>
+                            <a href="mailto:com@azc.archi">com@azc.archi</a>
+                            <br/>
+                            <a href="mailto:jobs@azc.archi">jobs@azc.archi</a>
+                        </p>
+                        <p>credits : AZC 2018</p>
+                        <a href="">mentions légales</a>
+                    </div>
+                    <div class="social-link">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/linkedin.png"/>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png"/>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png"/>
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/pinterest.png"/>
+                    </div>
+                </div>
+                <div class="col-md-6 col-12">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/carte.jpg"/>
+                </div>
             </div>
         </footer>
         <?php wp_footer(); ?>
