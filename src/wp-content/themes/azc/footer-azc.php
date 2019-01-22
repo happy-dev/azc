0c855298f59c1b4476f24cb9ccf833b360397ff9
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2019-01-22 21:34:40 +0100

    corrections footer page AZC

--- src/wp-content/themes/azc/footer-azc.php
+++ src/wp-content/themes/azc/footer-azc.php
@@ -1,33 +1,26 @@
         <footer class="footer-bg-white" id="contact">
             <div class="block-footer-AZC row">
-                <div class="col-xl-3 col-sm-5 col-12">
+                <div class="col-xl-2 col-sm-5 col-12 d-flex flex-column justify-content-between">
                     <p>CONTACT</p>
                     <div class="footer-contact">
-                        <p>
-                            15/17 rue vulpian
-                            <br/>
-                            75013 Paris
-                            <br/>
-                            T +33 1 55 25 25 25
-                            <br/>
-                            F +33 1 43 73 02 82
-                            <a href="mailto:contact@azc.archi">contact@azc.archi</a>
-                            <br/>
-                            <a href="mailto:com@azc.archi">com@azc.archi</a>
-                            <br/>
-                            <a href="mailto:jobs@azc.archi">jobs@azc.archi</a>
-                        </p>
-                        <p>credits : AZC 2018</p>
+                        <p>15/17 rue vulpian<br/>
+                        75013 Paris</p>
+                        <p class="mt-3">T +33 1 55 25 25 25<br/>
+                        F +33 1 43 73 02 82</p>
+                        <p class="mt-3"><a href="mailto:contact@azc.archi">contact@azc.archi</a><br/>
+                        <a href="mailto:com@azc.archi">com@azc.archi</a><br/>
+                        <a href="mailto:jobs@azc.archi">jobs@azc.archi</a></p>
+                        <p class="mt-3">credits : AZC 2018</p>
                         <a href="">mentions légales</a>
                     </div>
-                    <div class="social-link">
+                    <div class="social-link d-flex justify-content-between">
                         <img src="<?php echo get_template_directory_uri(); ?>/img/linkedin.png"/>
                         <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png"/>
                         <img src="<?php echo get_template_directory_uri(); ?>/img/instagram.png"/>
                         <img src="<?php echo get_template_directory_uri(); ?>/img/pinterest.png"/>
                     </div>
                 </div>
-                <div class="col-xl-3 col-sm-6 col-12">
+                <div class="col-xl-4 col-sm-6 col-12">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/carte.jpg"/>
                 </div>
                 <div class="col-xl-6 col-sm-1 col-12 text-uppercase text-right d-flex flex-column justify-content-end haut">Haut</div>
