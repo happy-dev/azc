a2d08607b893996f32358a43e82dd864d7a2b90e
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2019-02-01 08:31:09 +0100

    page news - init de la structure

--- src/wp-content/themes/azc/views/news.php
+++ src/wp-content/themes/azc/views/news.php
@@ -16,20 +16,36 @@ get_header(); ?>
     $news = new \WP_Query(array(
         'post_type' => 'postnews',
         'post_status' => 'publish',
-        'orderby' => 'title',
+        'orderby' => 'date',
         'order' => 'DESC',
     ));
 
     if ( $news->have_posts() ): ?>
         <ul class="news-list">
-            <?php while ( $news->have_posts() ) : $news->the_post();
-                echo '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
-            endwhile; ?>
+            <?php while ( $news->have_posts() ) : $news->the_post(); ?>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="owl-carousel owl-theme col-lg-6 col-12">
+                            <?php while( have_rows('slider_all_pictures') ): the_row();
+                                $image = get_sub_field('slider_one_picture'); ?>
+                                <div class="item">
+                                    <img src="<?php echo $image['url']; ?>" alt="<?php echo get_the_title(); ?>" />
+                                </div>
+                            <?php endwhile; ?>
+                        </div>
+                        <div class="col-lg-6 col-12">
+                            <?php echo '<li>'.get_the_title().'</li>' ;?> 
+                            <p> <?php echo the_field('single-text') ;?></p>                        
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; ?>
         </ul>
     <?php endif;
     wp_reset_postdata();
 
     ?>
+    
 
     </main><!-- .site-main -->
 </section><!-- .content-area -->
