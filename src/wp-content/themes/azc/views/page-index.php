9c50740124e37c096ece9b3facd26f682c638a4d
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2019-02-08 22:33:30 +0100

    page index : ui

--- src/wp-content/themes/azc/views/page-index.php
+++ src/wp-content/themes/azc/views/page-index.php
@@ -30,8 +30,7 @@ get_header();?>
 
                             <?php while ( $postsIndex->have_posts() ) : $postsIndex->the_post(); ?>
                                 <li>
-                                    <a href="<?php echo get_permalink(); ?>"> <?php echo get_the_title(); ?></a>
-                                    <br />
+                                   <div class="title"><?php echo get_the_title(); ?></div>
                                     <span>par <?php echo get_the_author(); ?></span>
                                     <p><?php echo get_the_content(); ?></p>
                                 </li>
