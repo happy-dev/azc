abbc3d5a499db14c77eba7e426e895ec4400e729
Nadir <nadir@happy-dev.fr>
2019-02-28 09:02:27 +0100

    wip menu single Works

--- src/wp-content/themes/azc/views/works.php
+++ src/wp-content/themes/azc/views/works.php
@@ -45,7 +45,7 @@ get_header();?>
                         echo '<li><a href="'.$term2Link.'">'.$term2->name.'</a></li>';
                     }
                 }
-                echo '<li>List</li>';
+                echo '<li><a href="#works-list" class="list-link">List</a></li>';
                 ?>
             </ul>
 
