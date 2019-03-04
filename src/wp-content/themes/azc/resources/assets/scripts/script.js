d622abf8f05d7da291a526c437952a1e38dd2d38
Nadir <nadir@happy-dev.fr>
2019-03-04 19:58:26 +0100

    légende slider single post work

--- src/wp-content/themes/azc/resources/assets/scripts/script.js
+++ src/wp-content/themes/azc/resources/assets/scripts/script.js
@@ -25,6 +25,8 @@ jQuery(function($){
         nav:true,
         margin:0,
         singleItem:true,
+        onInitialized: counter, //When the plugin has initialized.
+        onTranslated: counter, //When the translation of the stage has finished.
         responsive:{
             0:{
                 items:1
@@ -38,6 +40,18 @@ jQuery(function($){
         },
         onResized: resizeText,
     });
+    
+    function counter(event) {
+        var element   = event.target;         // DOM element, in this example .owl-carousel
+        var items     = event.item.count;     // Number of items
+        var item      = event.item.index + 1;     // Position of the current item
+
+        // it loop is true then reset counter from 1
+        if(item > items) {
+            item = item - items;
+        }
+        $('#counter').html(item+"/"+items);
+    }
 
     $('.about-more').click(function() {
         $('.about-text').removeClass("about-resize col-xl-6").addClass("column").height('auto');
