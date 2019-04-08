cdba389f8b06010772cf724bacaf2f0e8492c889
Nadir <nadir@happy-dev.fr>
2019-04-08 18:43:28 +0200

    add lang switcher

--- src/wp-content/themes/azc/footer-azc.php
+++ src/wp-content/themes/azc/footer-azc.php
@@ -17,6 +17,13 @@
                                 <a href="mailto:jobs@azc.archi">jobs@azc.archi</a></p>
                                 <p class="mt-3">credits : AZC 2018</p>
                                 <a href="<?php echo esc_url( home_url( '/mentions-legales' ) ); ?>">mentions légales</a>
+                                <?php $lang = get_bloginfo("language");
+                                if ( $lang == 'fr-FR' ) { ?>
+                                    <a href="<?php echo esc_url( home_url( '/mentions-legales' ) ); ?>">mentions légales</a>
+                                <?php }
+                                else if ( $lang == 'en-GB' ) { ?>
+                                    <a href="<?php echo esc_url( home_url( '/legal-notice' ) ); ?>">legal notice</a>
+                                <?php } ?>
                             </div>
                             <div class="social-link d-flex justify-content-between">
                                 <img src="<?php echo get_template_directory_uri(); ?>/img/linkedin.png"/>
