fa96b8d22f681720ceb0a1b1d49bf871d1b16c6a
Axel Viala <axel.viala@darnuria.eu>
2019-10-01 02:41:50 +0200

    Refactoring header.php

--- src/wp-content/themes/azc/header.php
+++ src/wp-content/themes/azc/header.php
@@ -1,38 +1,37 @@
+<?php
+
+$nav_data = [
+  'theme_location' => 'primary_navigation',
+  'container' => null,
+  'menu_class' => 'navbar-nav',
+];
+?>
 <!DOCTYPE html>
 
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">    
-    <?php wp_enqueue_script("jquery"); ?> 
-    <?php wp_head(); ?>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <?php wp_enqueue_script("jquery"); ?>
+  <?php wp_head(); ?>
 </head>
-    <body id="<?php echo $GLOBALS['templateName']; ?>">
-        <?php
-            if ( !is_front_page() && !is_home() ) { ?>
-                <div class="menu-fixed">
-                <?php if (has_nav_menu('primary_navigation')) {
-                    wp_nav_menu(array(
-                        'theme_location' => 'primary_navigation',
-                        'container' => null,
-                        'menu_class' => 'navbar-nav',
-                    ));
-                }
-                ?>
-                </div>
-                <div class="menu-single-work">
-                    <div class="text-uppercase menu-show">Menu</div>
-                    <div class="menu-hide hide"><img src="<?php echo get_template_directory_uri(); ?>/img/cancel.png"/></div>
-                    <div class="menu-fixed-single hide">            
-                        <?php
-                        wp_nav_menu(array(
-                            'theme_location' => 'primary_navigation',
-                            'container' => null,
-                            'menu_class' => 'navbar-nav',
-                        ));
-                        ?>
-                    </div>
-                </div>
-        <?php } ?>
+
+<body id="<?= $GLOBALS['templateName']; ?>">
+  <?php if (!is_front_page() && !is_home()): ?>
+    <div class="menu-fixed">
+      <?php if (has_nav_menu('primary_navigation')): ?>
+        <?php wp_nav_menu($nav_data); ?>
+      <?php endif; ?>
+    </div>
+
+    <div class="menu-single-work">
+      <div class="text-uppercase menu-show">Menu</div>
+      <div class="menu-hide hide"><img src="<?= get_template_directory_uri(); ?>/img/cancel.png" /></div>
+      <div class="menu-fixed-single hide">
+        <?php wp_nav_menu($nav_data); ?>
+      </div>
+    </div>
+  <?php endif; ?>
