f769e2226cdb2d5fea5f001ccea6dba6dc4a3a3e
Alex Bourlier <alex@paris.happy-dev.fr>
2022-09-27 11:29:44 +0200

    update: address added to AZC page

--- src/wp-content/themes/azc/views/azc.php
+++ src/wp-content/themes/azc/views/azc.php
@@ -9,6 +9,13 @@ get_header();
 
 <section id="primary" class="content-area mt-navb">
   <main id="main" class="site-main">
+    <div class="agency">
+      <h2>ATELIER ZÜNDEL CRISTEA</h2>
+      <h2>15/47 rue Vulpian</h2>
+      <h2>75013 Paris</h2>
+      <h2><a href="tel:+33155252494">+ 33 1 55 25 24 94</a></h2>
+      <section><?php the_content() ?></section>
+    </div>
     <div id="accordion" class="container-fluid"><?php
       $args = array(
 	'post_type' => 'azcsection',
