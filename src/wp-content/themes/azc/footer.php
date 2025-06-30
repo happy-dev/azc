d39af1f3f98276906179ffcde8d1e3ba2e6e7bf2
Alex Bourlier <alex@paris.happy-dev.fr>
2025-06-30 16:36:10 +0200

    html: address update

--- src/wp-content/themes/azc/footer.php
+++ src/wp-content/themes/azc/footer.php
@@ -1,7 +1,7 @@
     <footer>
       <?php wp_footer(); ?>
       <div id="centered-footer">
-	<a href="https://goo.gl/maps/KbW62qcShQkPga3a7" target="_blank"><img src="<?= get_template_directory_uri(); ?>/img/icon-loc.svg"/></a>
+	<a href="https://maps.app.goo.gl/3eiGYr2aVKT9uHtz9" target="_blank"><img src="<?= get_template_directory_uri(); ?>/img/icon-loc.svg"/></a>
 	<a href="mailto:agence@azc.archi" target="_blank"><img src="<?= get_template_directory_uri(); ?>/img/icon-mail.svg"/></a>
 	<a href="tel:+33155252494" target="_blank" data-toggle="tooltip" data-placement="top" title="+33 155 252 494"><img src="<?= get_template_directory_uri(); ?>/img/icon-tel.svg"/></a>
 	<a href="https://www.instagram.com/azc_architects/?hl=fr" target="_blank"><img src="<?= get_template_directory_uri(); ?>/img/icon-insta.svg"/></a>
--- src/wp-content/themes/azc/views/azc.php
+++ src/wp-content/themes/azc/views/azc.php
@@ -11,8 +11,8 @@ get_header();
   <main id="main" class="site-main">
     <div class="agency">
       <h2 class="zundel">ATELIER ZÜNDEL CRISTEA</h2>
-      <h2>15/17 rue Vulpian</h2>
-      <h2>75013 Paris</h2>
+      <h2>9 rue de la Pierre Levée</h2>
+      <h2>75011 Paris</h2>
       <h2><a href="tel:+33155252494">+ 33 1 55 25 24 94</a></h2>
       <section><?php the_content() ?></section>
     </div>
